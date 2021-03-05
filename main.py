cf66f0fa410cbae8d32d34d97aeb82ccb0b037fb
Vicky <vigneshwaran@tazwiz.com>
2021-03-05 09:52:20 -0500

    Day-36 completed

---
+++ main.py
@@ -0,0 +1,70 @@
+import requests
+from twilio.rest import Client
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = "VEZMQNCU2M6XHWO8"
+NEWS_API_KEY = "9b6bea9531d94c838af720ee5ae83a54"
+TWILIO_SID = "[YOUR TWILIO SID]"
+TWILIO_AUTH = "[YOUR TWILIO AUTH ID]"
+
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+print(day_before_yesterday_closing_price)
+
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "☝️"
+else:
+    up_down = "👇"
+
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+
+if abs(diff_percent) > 5:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    print(articles)
+    three_articles = articles[:3]
+    print(three_articles)
+
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+
+    client = Client(TWILIO_SID, TWILIO_AUTH)
+
+    for article in formatted_articles:
+        message = client.messages \
+            .create(
+            body=article,
+            from_='+14844696948',
+            to='[YOUR MOBILE NUMBER]'
+        )
+
+
+
+
+
+
+
